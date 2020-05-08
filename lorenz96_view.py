f61e8bf73f4d6e89f450527713647d27b525da33
Toyooka Daichi <s1920998@s.tsukuba.ac.jp>
2020-05-09 02:05:08 +0900

    add: colorbar

--- lorenz96_view.py
+++ lorenz96_view.py
@@ -11,23 +11,35 @@ import itertools
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib.colors import BoundaryNorm
+from matplotlib.ticker import MaxNLocator
 
 class lorenz96_score:
   def __init__(self, path:str, nx:int, nt:int):
     self.lorenz_data = self.csv2list(path).reshape(nt, nx)
     self.xs = np.arange(1, nx+1, step=1)
-    self.time_list = np.arange(nt, step=1)
+    self.ts = np.arange(nt, step=1)
     print('Call ..... Constract of lorenz96_score')
 
   def csv2list(self, path:str) -> np.ndarray:
     return np.genfromtxt(path, delimiter=",")
   
   def lorenz96_hovmoller(self) -> None:
-    ax = plt.gca()
-    cmap = plt.contourf(self.xs, self.time_list, self.lorenz_data, cmap=cm.coolwarm, extend='both')
+    fig, ax = plt.subplots()
+    levels = [ 
+                     -1.8, -1.6, -1.4, -1.2, -1.0,
+                      1.2,  1.4,  1.6,  1.8,  2.0,
+                      2.2,  2.4,  2.6,  2.8,  3.0 ]
+    cmap = plt.get_cmap('bwr')
+    norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+    cmap = plt.contourf(self.xs, self.ts, self.lorenz_data, levels, cmap=cmap, norm=norm, extend='both')
     ax.set_ylim(ax.get_ylim()[::-1])
+    fig.colorbar(cmap, ax=ax)
+    ax.set_title('contourf with levels', loc='left')
+
     plt.show()
 
 if __name__ == "__main__":
@@ -44,9 +56,7 @@ if __name__ == "__main__":
   # +++ class set
   # > lorenz96 cal. score
   score = lorenz96_score(data_path, nx, timestep)
-  print(score.lorenz_data.shape)
-  print(score.xs.shape)
-  print(score.time_list.shape)
+
   #---------------------------------------------------------- 
   # +++ draw func.
   # > hovmoller diagram
